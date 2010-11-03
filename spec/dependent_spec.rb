2302ea78358ff21e57dbdd0095399845175b26b4
grosser <grosser.michael@gmail.com>
2010-11-03 16:06:58 +0100

    make some permanent fixtures

--- spec/dependent_spec.rb
+++ spec/dependent_spec.rb
@@ -1,14 +1,18 @@
 $LOAD_PATH.unshift File.expand_path('../lib', File.dirname(__FILE__))
-require 'rubygems/dependent'
+require 'lib/rubygems/dependent' # normal require does not work once gem is installed, wtf...
 
 describe Gem::Dependent do
+  before do
+    Gem.stub!(:sources).and_return ['http://gemcutter.org']
+  end
+
   it 'finds dependencies for autotest' do
-    expected = [
-      ["3scale-3scale_ws_api", ["hpricot"]],
-      ["3scale-3scale_ws_api_for_ruby", ["hpricot"]],
-      ["Chrononaut-hostconnect", ["hpricot"]]
-    ]
-    dependencies = Gem::Dependent.find('hpricot', :fetch_limit => 100)
+    gem_source = URI.parse('http://gemcutter.org')
+    fixture = YAML.load(File.read('spec/fixtures/gemcutter_specs.yml'))
+    Gem::SpecFetcher.fetcher.should_receive(:load_specs).with(gem_source, 'specs').and_return(fixture)
+
+    expected = [["7digital", ["hoe"]], ["abingo_port", ["hoe"]], ["abundance", ["hoe"]], ["actionview-data", ["hoe"]]]
+    dependencies = Gem::Dependent.find('hoe', :fetch_limit => 100)
     dependencies.map{|name, deps| [name, deps.map{|d| d.name}] }.should == expected
   end
 
